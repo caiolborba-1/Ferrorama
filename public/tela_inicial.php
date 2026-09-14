2b4926d217ad536dd56b15dc9eb364984f21b38d
caio <caio_l_borba@etudante.sesisenai.org.br>
2026-09-14 12:53:08 -0300

    up - tela_user_semi-pronta

--- public/tela_inicial.php
+++ public/tela_inicial.php
@@ -18,13 +18,13 @@
 <div class="duas_colunas">
 
     <div class=" justify-content: center"> 
-        <div class="botao"></div> <br>
-        <div class="botao"></div>
+        <div class="botao">Trem 1 <br> Brasil - Japão</div> <br>
+        <div class="botao">Trem 3 <br> China - Malásia</div>
     </div>
    
     <div  justify-content: center>
-        <div class="botao"></a></div> <br>
-        <div class="botao"></div>
+        <div class="botao">Trem 2 <br> Peru - Equador</a></div> <br>
+        <div class="botao">Trem 4 <br> Maranhão - Rio</div>
     </div>
    
 
