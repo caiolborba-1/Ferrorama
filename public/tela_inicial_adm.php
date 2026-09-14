5933c72d566316a1af748c6d13eddfe8da5cf1c4
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-09-14 12:53:24 -0300

    add - animação na logo

--- public/tela_inicial_adm.php
+++ public/tela_inicial_adm.php
@@ -10,13 +10,9 @@
 
 <header>
 
-    <div class = "cabecalho" >
-        <img src="../assets/img/Trens certo.png" alt="" class="logo_cabecalho">
-        <h1><B>VOLTAR</B></h1>
-        <h1><B>INICIO</B></h1>
-        <h1><B>PERFIL</B></h1>
-        <h1><B>TRENS</B></h1>
-    </div>
+    <?php
+    include "navbar.php";
+    ?>
 
 </header>
 
