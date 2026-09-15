5650aa831da2c1e58e921cf180fb68aa1e2c3ec2
FelipeZuquetti <felipe_z_pereira@estudante.sesisenai.org.br>
2026-09-15 20:49:15 -0300

    up - pequena alteração na navbar

--- public/navbar.php
+++ public/navbar.php
@@ -1,7 +1,7 @@
  <div class = "cabecalho" >
         <img src="../assets/img/Trens certo.png" alt="" class="logo_cabecalho">
-        <h1><B>VOLTAR</B></h1>
-        <h1><B>INICIO</B></h1>
-        <h1><B>PERFIL</B></h1>
-        <h1><B>TRENS</B></h1>
+        <h1 class="afundar"><B>VOLTAR</B></h1>
+        <h1 class="afundar"><B>INICIO</B></h1>
+        <h1 class="afundar"><B>PERFIL</B></h1>
+        <h1 class="afundar"><B>TRENS</B></h1>
 </div>
