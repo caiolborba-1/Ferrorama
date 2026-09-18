ed51adf9c4e52182e00e5a5184f73731e50303a6
matheustomczak-dev <matheus_tomczak@estudante.sesisenai.org.br>
2026-09-18 11:30:59 -0300

    up- adicionei um grafico na pagina de sensores de combustivel

--- public/sensores_combustivel.php
+++ public/sensores_combustivel.php
@@ -2,51 +2,65 @@
 
     include "../infra/conexao.php";
 
-
-
 ?>
+
 <html lang="en">
 
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../assets/styles/styles.css">
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
         <title>Sensor de Combustivel</title>
     </head>
 
-    <body class="">
+    <body class="aling-items-center">
 
         <?php include 'navbar.php'; ?>
 
-        <div class="">
-            <div class="">
-                <div>
-                    <br>
-                    <h1>Combustivel</h1>
-                </div>
+        <div class="container">
+
+            <div>
+                <br>
+                <br>
+                <h1>Combustivel</h1>
+                <br><br>
             </div>
 
-            <div class=" display-flex align-items-center gap-5 ">
-                <div>
-                    <h1>ID: Trem 1</h1> <br>
-                    <h1>ID: Trem 2</h1> <br>
-                    <h1>ID: Trem 3</h1> <br>
-                    <h1>ID: Trem 4</h1> <br>
-                </div>
-
-                <div>
-                    <h1>combustivel: X L</h1> <br>
-                    <h1>combustivel: X L</h1> <br>
-                    <h1>combustivel: X L</h1> <br>
-                    <h1>combustivel: X L</h1> <br>
-
-                </div>
-
+            <div style="width: 80%; margin: auto;">
+                <canvas id="graficoCombustivel"></canvas>
             </div>
+
+            <script>
+                const ctx = document.getElementById('graficoCombustivel');
+
+                new Chart(ctx, {
+                    type: 'bar',
+
+                    data: {
+                        labels: ['Trem 1', 'Trem 2', 'Trem 3', 'Trem 4'],
+
+                        datasets: [{
+                            label: 'Combustível (L)',
+                            data: [800, 500, 1000, 650]
+                        }]
+                    },
+
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            </script>
 
         </div>
-
 
     </body>
 
