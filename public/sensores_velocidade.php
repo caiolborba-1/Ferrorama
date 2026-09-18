779b0141ed6bed6e7befda5bcb721c43c6476b26
HLombardi <henrique_lombardi@estudante.sesisenai.org.br>
2026-09-18 11:51:46 -0300

    up sensor de velocidade

--- public/sensores_velocidade.php
+++ public/sensores_velocidade.php
@@ -10,7 +10,7 @@
     <?php include 'navbar.php'; ?>
 
     <div class="display2">
-
+        <div>
         <br>
 
         <h1>Velocidade</h1>
@@ -42,81 +42,83 @@
                 <canvas id="graficoVelocidade"></canvas>
             </div>
 
-        </div>
+        
 
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-        <script>
+            <script>
 
-            const grafico = document.getElementById('graficoVelocidade');
+                const grafico = document.getElementById('graficoVelocidade');
 
-            new Chart(grafico, {
-                type: 'line',
+                new Chart(grafico, {
+                    type: 'line',
 
-                data: {
-                    labels: [
-                        '0', '1', '2', '3',
-                        '4', '5', '6', '7',
-                        '8', '9', '10', '11'
-                    ],
-
-                    datasets: [{
-                        label: 'Velocidade',
-
-                        data: [
-                            30,
-                            70,
-                            105,
-                            130,
-                            145,
-                            175,
-                            135,
-                            148,
-                            217,
-                            195,
-                            95,
-                            55
+                    data: {
+                        labels: [
+                            '0', '1', '2', '3',
+                            '4', '5', '6', '7',
+                            '8', '9', '10', '11'
                         ],
 
-                        borderWidth: 2,
+                        datasets: [{
+                            label: 'Velocidade',
 
-                        pointRadius: 4,
+                            data: [
+                                30,
+                                70,
+                                105,
+                                130,
+                                145,
+                                175,
+                                135,
+                                148,
+                                217,
+                                195,
+                                95,
+                                55
+                            ],
 
-                        tension: 0
-                    }]
-                },
+                            borderWidth: 2,
 
-                options: {
-                    responsive: true,
+                            pointRadius: 4,
 
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: 'Velocidade dos Trens'
-                        }
+                            tension: 0
+                        }]
                     },
 
-                    scales: {
-                        x: {
+                    options: {
+                        responsive: true,
+
+                        plugins: {
                             title: {
                                 display: true,
-                                text: 'Tempo'
+                                text: 'Velocidade dos Trens'
                             }
                         },
 
-                        y: {
-                            beginAtZero: true,
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: 'Tempo'
+                                }
+                            },
 
-                            title: {
-                                display: true,
-                                text: 'Velocidade (km/h)'
+                            y: {
+                                beginAtZero: true,
+
+                                title: {
+                                    display: true,
+                                    text: 'Velocidade (km/h)'
+                                }
                             }
                         }
                     }
-                }
-            });
+                });
 
-        </script>
+            </script>
+            
+        </div></div>
     </div>
 </body>
 </html>
