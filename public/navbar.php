45a00001eccbb1bd0306d0eec4cf02fe3e3e411c
HLombardi <henrique_lombardi@estudante.sesisenai.org.br>
2026-09-21 12:42:15 -0300

    add- botao perfil

--- public/navbar.php
+++ public/navbar.php
@@ -2,7 +2,7 @@
         <a href="ronaldo.html" onclick="carregarPagina('ronaldo.html'); return false;"><img src="../assets/img/Trens certo.png" alt="" class="logo_cabecalho"></a>
         <button onclick="history.back()" class = "botao_cabecalho afundar"><b>VOLTAR</b></button>
         <button onclick="location.href='tela_inicial_adm.php'" class = "botao_cabecalho afundar"><b>INÍCIO</b></button>
-        <h1 class="afundar"><B>PERFIL</B></h1>
+        <a href=""><h1 class="afundar"><B>PERFIL</B></h1></a>
         <h1 class="afundar"><B>TRENS</B></h1>
 
         <div id="relogio" class="relogio">00:00:00</div>
---
+++ public/perfil.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="../assets/styles/styles.css">
+</head>
+<body>
+    <?php
+    include "navbar.php"
+    ?>
+</body>
+</html>
