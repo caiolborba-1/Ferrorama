7e70ffcfcda603284eeb494853f782545502ed06
caio <caio_l_borba@etudante.sesisenai.org.br>
2026-09-21 11:53:12 -0300

    up - telas_trens_Prontos

---
+++ public/tela_localização_trem4.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=div, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+     <script src="../scripts/relogio_navbar.js"></script>
+     <link rel="stylesheet" href="../assets/styles/styles.css">
+     <script src="../scripts/relogio_navbar.js"></script>
+    <title>Trem- 1</title>
+</head>
+
+<body class="">
+
+    <?php
+    include "navbar.php"
+    ?>
+
+<br><br>
+
+<div class="tela_trem_1">
+  
+
+ 
+    <div class="lado_esquerdo">
+        <h2>Localização do Trem</h2>
+        <h3>Horários brasil - Japão</h3>
+
+        <div class="botoes_trem1">
+            <div class="botao">08:00</div>
+            <div class="botao">20:00</div>
+        </div>
+    </div>
+   
+    <div class="lado_direito">
+     <img id="trilhos" src="../assets/img/trilhosreal.webp" alt="">
+</div>
+
+
+
+
+</div>  
+
+
+    
+
+
+    
+    
+ 
+
+
+
+   
+
+</body>
+
+</html>
