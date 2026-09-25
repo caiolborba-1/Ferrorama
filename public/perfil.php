61d4064a4b3e98769cde69da0541633d88c72eab
HLombardi <henrique_lombardi@estudante.sesisenai.org.br>
2026-09-25 12:12:42 -0300

    tabela perfil

--- public/perfil.php
+++ public/perfil.php
@@ -10,9 +10,44 @@
     include "navbar.php"
     ?>
 
-    <div class="display_flex branco">
+    <div class="justify-content-center align-items-center">
 
-    </div>
+            <div class="card-container">
+        <h1>Sensores Cadastrados</h1>
+
+        <table class="custom-table">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Senha</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Sensor 1</td>
+                <td>Estação Norte</td>
+                <td>Velocímetro</td>
+                <td>Trem 1</td>
+                <td><button class="edit-button">Editar</button></td>
+                <td><button class="delete-button">Excluir</button></td>
+            </tr>
+            <tr>
+                <td>Sensor 2</td>
+                <td>Estação Sul</td>
+                <td>Pressão</td>
+                <td>Trem 2</td>
+                <td><button class="edit-button">Editar</button></td>
+                <td><button class="delete-button">Excluir</button></td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+
+        </div>
 
 </body>
 </html>
